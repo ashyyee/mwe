6b62c59750eceb145a8432355369b5ad2210c3db
Ash <kksunnyyy@gmail.com>
2026-08-05 18:07:41 -0300

    .

---
+++ script.js
@@ -0,0 +1,35 @@
+const sim = document.getElementById("sim");
+const nao = document.getElementById("nao");
+const imagem = document.getElementById("imagem");
+
+let tamanhoSim = 1;
+let tamanhoNao = 1;
+
+const gifs = [
+    "imagens/gif1.gif",
+    "imagens/gif2.gif",
+    "imagens/gif3.gif"
+];
+
+let indice = 0;
+
+nao.addEventListener("click", () => {
+
+    tamanhoSim += 0.2;
+    sim.style.transform = `scale(${tamanhoSim})`;
+
+    tamanhoNao -= 0.15;
+    if (tamanhoNao < 0.2) {
+        tamanhoNao = 0.2;
+    }
+    nao.style.transform = `scale(${tamanhoNao})`;
+
+    if (indice < gifs.length - 1) {
+        indice++;
+        imagem.src = gifs[indice];
+    }
+});
+
+sim.addEventListener("click", () => {
+    window.location.href = "carta.html";
+});
